c51f9a8091701219f6ef1a05140a288390a99fe4
Hritik Raj <hritikraj0203@gmail.com>
2024-11-02 13:27:06 +0530

    Pushing the main code

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -23,13 +23,15 @@ function Login() {
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
                 placeholder='username' />
-<br />
+                <br />
+
             <input
                 type="text"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 placeholder='password' />
-<br />
+                <br />
+
             <button onClick={handleSubmit}>Submit</button>
 
         </div>
